9f389db101fa57b259f09d3d06730beeb169e69e
Seth Krasnianski <seth@figmints.com>
2013-05-12 13:13:44 -0400

    Style and header update

--- header.php
+++ header.php
@@ -2,8 +2,8 @@
 			<div id="center">
 				<a href="index.php"><img src="images/logo.png" alt="SJK | Web Developer &amp; Designer" title="SJK | Web Developer &amp; Designer" width="270" height="120" /></a>
 				<nav>
-					<a href="index.php#home">HOME</a>
-					<a href="index.php#about">ABOUT</a>
+					<a href="/home">HOME</a>
+					<a href="/about">ABOUT</a>
 					<a href="index.php#portfolio">PORTFOLIO</a>
 					<a href="index.php#contact">CONTACT</a>
 				</nav>
